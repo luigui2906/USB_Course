b357d7575ca5edb5ef92637b28aadf89d3595d91
Luis Roldao <luigui2906@gmail.com>
2017-12-05 17:14:03 +0100

    New functions added in order to do the 2nd exercise for the students (calculate rotation matrix to align two different pointclouds from their normal vectors)

--- Ransac_PCL.py
+++ Ransac_PCL.py
@@ -16,7 +16,7 @@ def read_pcd_file(input_filename):
 
 def write_pointcloud_file(pointcloud, output_path):
     output_pointcloud = pcl.PointCloud()
-    output_pointcloud.from_array(pointcloud)
+    output_pointcloud.from_array(np.float32(pointcloud))
     output_pointcloud.to_file(output_path)
     return
 
@@ -31,7 +31,7 @@ def viewer_pointcloud(pointcloud):
 def viewer_original_vs_ransac_pointcloud_vs_plane(ransac_pcl, original_pcl, plane_model):
     sensor_range = 120.0
     mlab.figure(bgcolor=(1, 1, 1))
-    x, y = np.ogrid[-sensor_range:sensor_range:1, -sensor_range:sensor_range:1]
+    x, y = np.ogrid[-sensor_range+50:sensor_range+50:1, -sensor_range:sensor_range:1]
     mlab.points3d(original_pcl[:, 0], original_pcl[:, 1], original_pcl[:, 2], color=(0, 0, 0), mode='point')
     mlab.points3d(ransac_pcl[:, 0], ransac_pcl[:, 1], ransac_pcl[:, 2], color=(1, 0, 0), mode='point')
     mlab.surf(x, y, (-plane_model[3] - (plane_model[0]*x) - (plane_model[1]*y)) / plane_model[2],
@@ -95,12 +95,48 @@ def random_sampling_consensus(pointcloud, numb_iterations, threshold):
     return ransac_pointcloud, plane_model
 
 
+def calculate_rotation_matrix(plane_model1, plane_model2):
+    I = np.identity(3)
+    normal_vector1 = normalize_vector(np.array([plane_model1[0], plane_model1[1], plane_model1[2]]))
+    normal_vector2 = normalize_vector(np.array([plane_model2[0], plane_model2[1], plane_model2[2]]))
+    cross_nv1nv2 = np.cross(normal_vector1, normal_vector2)
+    v1, v2, v3 = cross_nv1nv2[0], cross_nv1nv2[1], cross_nv1nv2[2]
+    Vperp = np.array([[0, -v3, v2], [v3, 0, -v1], [-v2, v1, 0]])
+    c = np.dot(normal_vector1, np.transpose(normal_vector2))
+    Result = I + Vperp + (np.dot(Vperp, Vperp)*(1/1+c))
+    Result = np.c_[Result, np.zeros(Result.shape[0])]
+    Result = np.r_[Result, [np.array([0, 0, 0, 1])]]
+    return Result
+
+
+def transform_pointcloud(transf_matrix, pointcloud):
+    return np.delete(np.transpose(np.dot(transf_matrix,
+                                         np.transpose(np.c_[pointcloud, np.ones(pointcloud.shape[0])]))), 3, axis=1)
+
+
+def normalize_vector(vector):
+    return vector/np.linalg.norm(vector)
+
+
 def main():
-    input_path = 'C:/Users/lroldaoj/Documents/PycharmProjects/Ransac_PCL/pointcloud_example.pcd'
-    pointcloud = read_pcd_file(input_path)
-    viewer_pointcloud(pointcloud)
-    ransac_pointcloud, plane_model = random_sampling_consensus(pointcloud, 100, 0.2)
-    viewer_original_vs_ransac_pointcloud_vs_plane(ransac_pointcloud, pointcloud, plane_model)
+    # Exercise 1
+    # input_path = 'C:/Users/lroldaoj/Documents/PycharmProjects/Ransac_PCL/pointcloud_example.pcd'
+    # pointcloud = read_pcd_file(input_path)
+    # viewer_pointcloud(pointcloud)
+    # ransac_pointcloud, plane_model = random_sampling_consensus(pointcloud, 100, 0.2)
+    # viewer_original_vs_ransac_pointcloud_vs_plane(ransac_pointcloud, pointcloud, plane_model)
+
+    #Exercise 2
+    pointcloud1 = read_pcd_file('C:/Users/lroldaoj/Documents/PycharmProjects/Ransac_PCL/PointCloud1.pcd')
+    pointcloud2 = read_pcd_file('C:/Users/lroldaoj/Documents/PycharmProjects/Ransac_PCL/PointCloud2rot.pcd')
+    viewer_original_vs_ransac_pointcloud_vs_plane(pointcloud1, pointcloud2, np.array([12.40711689, -11.24007034,
+                                                                                      -568.33337402, -999.6496582]))
+    ransac_pointcloud1, plane_model1 = random_sampling_consensus(pointcloud1, 100, 0.2)
+    ransac_pointcloud2, plane_model2 = random_sampling_consensus(pointcloud2, 100, 0.2)
+    Transf_matrix = calculate_rotation_matrix(plane_model1, plane_model2)
+    print(Transf_matrix)
+    pointcloud2_fixed = transform_pointcloud(Transf_matrix, pointcloud2)
+    viewer_original_vs_ransac_pointcloud_vs_plane(pointcloud1, pointcloud2_fixed, plane_model1)
 
 
 if __name__ == '__main__':
